9f5c5253bbed86380ca864f40dceac524ee88d9a
Amanda Steigerwalt <asteig@gmail.com>
2019-04-16 11:44:52 -0700

    Added 'description' and 'price' indexes to Products store

--- indexed-db-lab/app/js/main.js
+++ indexed-db-lab/app/js/main.js
@@ -18,30 +18,104 @@ var idbApp = (function() {
 
   // TODO 2 - check for support
 
-  var dbPromise = idb.open('couches-n-things', 1, function(upgradeDb) {
-  switch (upgradeDb.oldVersion) {
-    case 0:
-      // a placeholder case so that the switch block will 
-      // execute when the database is first created
-      // (oldVersion is 0)
-    case 1:
-      console.log('Creating the products object store');
-      upgradeDb.createObjectStore('products', {keyPath: 'id'});
+  var dbPromise = idb.open('couches-n-things', 4, function(upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+        // a placeholder case so that the switch block will 
+        // execute when the database is first created
+        // (oldVersion is 0)
+      case 1:
+        console.log('Creating the products object store');
+        upgradeDb.createObjectStore('products', {keyPath: 'id'});
+      case 2:
+        console.log('Creating a name index');
+        var store = upgradeDb.transaction.objectStore('products');
+        store.createIndex('name', 'name', {unique: true});
 
-    case 2:
-    // TODO 4.1 - create 'name' index
-      console.log('Creating \'name\' index...');
+      // TODO 4.2 - create 'price' and 'description' indexes
+      case 3:
+        console.log('Creating \'price\' and \'description\' indexes');
+        var store = upgradeDb.transaction.objectStore('products');
+        store.createIndex('description', 'description');
+        store.createIndex('price', 'price');
+      // TODO 5.1 - create an 'orders' object store
 
-    // TODO 4.2 - create 'price' and 'description' indexes
-
-    // TODO 5.1 - create an 'orders' object store
-
-  }
-});
+    }
+  });
 
   function addProducts() {
 
     // TODO 3.3 - add objects to the products store
+    dbPromise.then(function(db) {
+      var tx = db.transaction('products', 'readwrite');
+      var store = tx.objectStore('products');
+      var items = [
+        {
+          name: 'Couch',
+          id: 'cch-blk-ma',
+          price: 499.99,
+          color: 'black',
+          material: 'mahogany',
+          description: 'A very comfy couch',
+          quantity: 3
+        },
+        {
+          name: 'Armchair',
+          id: 'ac-gr-pin',
+          price: 299.99,
+          color: 'grey',
+          material: 'pine',
+          description: 'A plush recliner armchair',
+          quantity: 7
+        },
+        {
+          name: 'Stool',
+          id: 'st-re-pin',
+          price: 59.99,
+          color: 'red',
+          material: 'pine',
+          description: 'A light, high-stool',
+          quantity: 3
+        },
+        {
+          name: 'Chair',
+          id: 'ch-blu-pin',
+          price: 49.99,
+          color: 'blue',
+          material: 'pine',
+          description: 'A plain chair for the kitchen table',
+          quantity: 1
+        },
+        {
+          name: 'Dresser',
+          id: 'dr-wht-ply',
+          price: 399.99,
+          color: 'white',
+          material: 'plywood',
+          description: 'A plain dresser with five drawers',
+          quantity: 4
+        },
+        {
+          name: 'Cabinet',
+          id: 'ca-brn-ma',
+          price: 799.99,
+          color: 'brown',
+          material: 'mahogany',
+          description: 'An intricately-designed, antique cabinet',
+          quantity: 11
+        }
+      ];
+      return Promise.all(items.map(function(item) {
+          console.log('Adding item: ', item);
+          return store.add(item);
+        })
+      ).catch(function(e) {
+        tx.abort();
+        console.log(e);
+      }).then(function() {
+        console.log('All items added successfully!');
+      });
+    });
 
   }
 
